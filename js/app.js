a2870723977c06163504498627b1385c10b13d0b
perry kong <apk29@hotmail.com>
2017-06-20 09:59:04 -0700

    third, fix map styling and updated formatting

--- js/app.js
+++ js/app.js
@@ -1,58 +1,225 @@
 // first goal: display a list with location names using Knockout.js (add the map later)
 // hard coded Array of location objects
-var locations = [
-        {
-            name: 'wholefoods', 
-            lat: 37.812570, 
-            lng: -122.260909
-        },
-        {
-            name: 'Lake Chalet', 
-            lat: 37.802232, 
-            lng: -122.261591
-        },
-        {
-            name: 'Oakland Chinatown', 
-            lat: 37.798560, 
-            lng: -122.269270
-        },
-        {
-            name: 'Fox Theater', 
-            lat: 37.808117, 
-            lng: -122.269768
-        },
-        {
-            name: 'Great Western Power Company',
-            lat: 37.809787, 
-            lng: -122.2701297
-        },
-    ];
+var locations = [{
+        name: 'wholefoods',
+        lat: 37.812570,
+        lng: -122.260909
+    },
+    {
+        name: 'Lake Chalet',
+        lat: 37.802232,
+        lng: -122.261591
+    },
+    {
+        name: 'Oakland Chinatown',
+        lat: 37.798560,
+        lng: -122.269270
+    },
+    {
+        name: 'Fox Theater',
+        lat: 37.808117,
+        lng: -122.269768
+    },
+    {
+        name: 'Great Western Power Company',
+        lat: 37.809787,
+        lng: -122.2701297
+    },
+];
 
 function initMap() {
+    var styles = [{
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#ebe3cd'
+                }]
+            },
+            {
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#523735'
+                }]
+            },
+            {
+                elementType: 'labels.text.stroke',
+                stylers: [{
+                    color: '#f5f1e6'
+                }]
+            },
+            {
+                featureType: 'administrative',
+                elementType: 'geometry.stroke',
+                stylers: [{
+                    color: '#c9b2a6'
+                }]
+            },
+            {
+                featureType: 'administrative.land_parcel',
+                elementType: 'geometry.stroke',
+                stylers: [{
+                    color: '#dcd2be'
+                }]
+            },
+            {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#ae9e90'
+                }]
+            },
+            {
+                featureType: 'landscape.natural',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#dfd2ae'
+                }]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#dfd2ae'
+                }]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#93817c'
+                }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry.fill',
+                stylers: [{
+                    color: '#a5b076'
+                }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#447530'
+                }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#f5f1e6'
+                }]
+            },
+            {
+                featureType: 'road.arterial',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#fdfcf8'
+                }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#f8c967'
+                }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{
+                    color: '#e9bc62'
+                }]
+            },
+            {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#e98d58'
+                }]
+            },
+            {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry.stroke',
+                stylers: [{
+                    color: '#db8555'
+                }]
+            },
+            {
+                featureType: 'road.local',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#806b63'
+                }]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#dfd2ae'
+                }]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#8f7d77'
+                }]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'labels.text.stroke',
+                stylers: [{
+                    color: '#ebe3cd'
+                }]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#dfd2ae'
+                }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry.fill',
+                stylers: [{
+                    color: '#b9d3c2'
+                }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#92998d'
+                }]
+            }
+        ],
 
-    
-	
-         // Constructor creates a new map - only center and zoom are required.
+
+        // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 37.813331, lng: -122.261801},
-          zoom: 13,
-          /*styles: styles,*/
-          mapTypeControl: false
+            center: {
+                lat: 37.813331,
+                lng: -122.261801
+            },
+            zoom: 13,
+            styles: styles,
+            mapTypeControl: false
         });
 };
-        
 
-var AppViewModel = function () {
+
+var AppViewModel = function() {
     var self = this;
-        self.markers = ko.observableArray([]);
-        self.allLocations = ko.observableArray(locations);
-         
-    self.filter =  ko.observable("");
+    self.markers = ko.observableArray([]);
+    self.allLocations = ko.observableArray(locations);
+
+    self.filter = ko.observable("");
     self.search = ko.observable("");
 };
 
- var appViewModel = new AppViewModel();
- ko.applyBindings(appViewModel);
+var appViewModel = new AppViewModel();
+ko.applyBindings(appViewModel);
 
 // https://github.com/udacity/ud864/blob/master/Project_Code_5_BeingStylish.html#L150
 
@@ -75,4 +242,4 @@ var AppViewModel = function () {
 // https://discussions.udacity.com/t/text-not-updating-with-search-box/182886/6
 
 // Apply the bindings aka activate KO
-// http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models#activating-knockout 
+// http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models#activating-knockout
